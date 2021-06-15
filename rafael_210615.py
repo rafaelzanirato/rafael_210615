36c66aa572f0d1a8fcf9ab51a5a64c4b71b70439
rafaelzanirato <84473402+rafaelzanirato@users.noreply.github.com>
2021-06-15 09:28:25 -0300

    Add files via upload

---
+++ rafael_210615.py
@@ -0,0 +1,30 @@
+# Question 2 e 3 
+for value in range(11):
+    print(value)
+# Queston 4 e 5
+list=["0","1","2","3","4","5","6","7","8","9","10"]
+print(list)
+# Question 6
+for value in range(10):
+   print("Rafael")
+# Question 7 e 8
+squares=[]
+for value in range(1,11):
+    squares.append("Rafael")
+print(squares)
+# Questions 9 e 10
+for value in range(5):
+    print("Rafael".lower())
+for value in range(5):
+    print("Rafael".upper())
+# Questions 11, 12 e 13
+for value in range(10):
+    print("Rafael".lower())
+for value in range(5,10):
+    print("Rafael".upper())
+# Question 14
+
+
+
+
+
